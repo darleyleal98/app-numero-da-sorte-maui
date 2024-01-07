c0234a2a948083fc6500badc92d5e76255dc6a9a
Darley Leal <darleyleal98@gmail.com>
2024-01-07 14:09:19 -0300

    Finalizado as telas do aplicativo.

--- AppNumeroDaSorte/App.xaml.cs
+++ AppNumeroDaSorte/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace AppNumeroDaSorte
+﻿using NumeroDaSorte;
+
+namespace AppNumeroDaSorte
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MainPage();
         }
     }
 }
